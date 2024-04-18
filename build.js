795bf1c00ea387102adbb652150380296967cdac
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2024-04-18 20:46:21 +0000

    Add module support.

--- build.js
+++ build.js
@@ -4,12 +4,24 @@ build({
   entryPoints: [
     "./src/index.ts"
   ],
-  outfile: "dist/jstogd.js",
-  bundle: true,
+  outfile: "dist/jstogd.min.js",
   platform: "browser",
+  bundle: true,
   minify: true,
   target: "es2020",
   globalName: "jsToGd"
+});
+
+build({
+  entryPoints: [
+    "./src/index.ts"
+  ],
+  outfile: "dist/jstogd.esm.js",
+  platform: "browser",
+  format: "esm",
+  bundle: true,
+  minify: true,
+  target: "es2020"
 });
 
 build({
